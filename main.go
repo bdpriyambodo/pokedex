c782354be1735745035b3a931dc2712d00e9ce3b
bdpriyambodo <73590204+bdpriyambodo@users.noreply.github.com>
2026-03-02 09:36:40 +0700

    adding repl

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Hello, World!")
-
+	// fmt.Println("Hello, World!")
 	// TestCleanInput("  hello  world  ")
+
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Pokedex > ")
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 {
+			break
+		}
+
+		fmt.Println("Your command was:", cleanInput(line)[0])
+
+		fmt.Print("Pokedex > ")
+	}
+}
 
 func cleanInput(text string) []string {
 	// separator := " "
 	// result := strings.Split(text, separator)
 	lowerCaseText := strings.ToLower((text))
 	result := strings.Fields(lowerCaseText)
-	fmt.Println("Result: ", result)
-	fmt.Println("Length: ", len(result))
+	// fmt.Println("Result: ", result)
+	// fmt.Println("Length: ", len(result))
 	return result
 }
